b836ac93419fd699754bcfc20541cef951cec192
Branden Brown <20bbrown14@gmail.com>
2020-08-16 12:35:18 -0500

    Add route for submitting apps

---
+++ app/models/application.rb
@@ -0,0 +1,13 @@
+class Application < ApplicationRecord
+
+  #def initialize(application)
+  #  @inGameName = application[:inGameName]
+  #  @discordUsername = application[:discordUsername]
+  #  @age = application[:age]
+  #  @location = application[:location]
+  #  @joinReason = application[:joinReason]
+  #  @playStyle = application[:playStyle]
+  #  @freeTime = application[:freeTime]
+  #  @source = application[:source]
+  #end
+end
